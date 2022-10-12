a5f8e0e9af08a6cf0f70b490ccbc3532e4015254
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-10-12 14:49:27 -0300

    Made UnitTest() test all the already made methods as well as GainXP()

--- HealthSystem/Program.cs
+++ HealthSystem/Program.cs
@@ -14,29 +14,39 @@ namespace HealthSystem
         static int shield;
         static int maxShield = 100;
         static int lives;
-        static int MaxLives = 99;
+        static int maxLives = 99;
+        static int defaultLives = 3;
         static int lvl;
         static int maxLvl = 99;
+        static int defaultLvl = 1;
         static int xp;
         static int xpToLvlUp = 100;
+        static int defaultXP = 0;
+        static string status;
 
         static void Main(string[] args)
         {
-
+            UnitTest();
         }
 
         static void Reset()
         {
             health = maxHealth;
             shield = maxShield;
-            lives = 3;
-            lvl = 1;
-            xp = 0;
+            lives = defaultLives;
+            lvl = defaultLvl;
+            xp = defaultXP;
+
+            DetermineStatus();
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("RESET");
+            Console.WriteLine();
         }
 
-        static string DetermineStatus()
+        static void DetermineStatus()
         {
-            string status;
 
             if(health >= 0 && health <= 10)
             {
@@ -62,8 +72,6 @@ namespace HealthSystem
                 status = "Something went wrong in DetermineStatus()";
             }
 
-            return status;
-
         }
 
         static void ShowHud()
@@ -72,8 +80,10 @@ namespace HealthSystem
             Console.WriteLine("-----------------------");
             Console.WriteLine("Shield: " + shield);
             Console.WriteLine("Health: " + health);
-            Console.WriteLine("Status: " + DetermineStatus());
+            Console.WriteLine("Status: " + status);
             Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("Level: " + lvl);
+            Console.WriteLine("XP: " + xp);
             Console.WriteLine("-----------------------");
             Console.WriteLine();
             Console.ReadKey(true);
@@ -108,6 +118,7 @@ namespace HealthSystem
             {
                 shield -= DMG;
             }
+            DetermineStatus();
         }
 
         static void GameOver()
@@ -117,21 +128,125 @@ namespace HealthSystem
 
         static void Heal(int toHeal)
         {
+            Console.WriteLine("Player gained " + toHeal.ToString() + " health");
             health += toHeal;
             if(health > maxHealth)
             {
                 health = maxHealth;
             }
+            DetermineStatus();
         }
 
         static void RegenShield(int toRegen)
         {
+            Console.WriteLine("Player gained " + toRegen.ToString() + " shield");
             shield += toRegen;
             if(shield > maxShield)
             {
                 shield = maxShield;
             }
         }
+
+        static void GainXP(int toGain)
+        {
+            Console.WriteLine("Player gained " + toGain + " XP");
+            xp += toGain;
+            while(xp >= xpToLvlUp)
+            {
+                xp -= xpToLvlUp;
+                if(lvl < maxLvl)
+                {
+                    Console.WriteLine("LEVEL UP!");
+                    lvl += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Already at max level");
+                }
+            }
+        }
+
+        static void UnitTest()
+        {
+            //take 50 dmg
+            Reset();
+            ShowHud();
+            TakeDMG(50);
+            ShowHud();
+
+            //take 150 dmg
+            Reset();
+            ShowHud();
+            TakeDMG(150);
+            ShowHud();
+
+            //take damage to show healing
+            Reset();
+            ShowHud();
+            Console.WriteLine("DEBUG: Player is going to be damaged and then healed");
+            TakeDMG(150);
+            ShowHud();
+            Heal(25);
+            ShowHud();
+
+            //take damage and then heal above max
+            Reset();
+            ShowHud();
+            Console.WriteLine("DEBUG: Player is going to be damaged and then healed above their max");
+            TakeDMG(150);
+            ShowHud();
+            Heal(150);
+            ShowHud();
+
+            //take damage and then regen shield
+            Reset();
+            ShowHud();
+            Console.WriteLine("DEBUG: Player is going to be damaged and then regain shield");
+            TakeDMG(50);
+            ShowHud();
+            RegenShield(25);
+            ShowHud();
+
+            //take damage and then regen shield above max
+            Reset();
+            ShowHud();
+            Console.WriteLine("DEBUG: Player is going to be damaged and regain shield above their max");
+            TakeDMG(50);
+            ShowHud();
+            RegenShield(75);
+            ShowHud();
+
+            //Gain XP
+            Reset();
+            ShowHud();
+            GainXP(50);
+            ShowHud();
+
+            //Gain XP enough to lvl up
+            Reset();
+            ShowHud();
+            GainXP(150);
+            ShowHud();
+
+            //Gain XP enough to lvl up multiple times
+            Reset();
+            ShowHud();
+            Console.WriteLine("DEBUG: Player is going to gain enough XP to level up multiple times in one go");
+            GainXP(350);
+            ShowHud();
+
+            //Gain XP enough to reach max lvl
+            Reset();
+            ShowHud();
+            Console.WriteLine("DEBUG: Player is going to reach max lvl");
+            GainXP(9950);
+            ShowHud();
+
+
+
+        }
+
+
 
 
 
