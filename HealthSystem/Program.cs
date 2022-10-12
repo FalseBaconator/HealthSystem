9ebe7de488d1216e369f894a32e361b7f422b498
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-10-12 14:01:52 -0300

    Made variables and Reset()

--- HealthSystem/Program.cs
+++ HealthSystem/Program.cs
@@ -8,8 +8,30 @@ namespace HealthSystem
 {
     internal class Program
     {
+
+        static int health;
+        static int maxHealth = 100;
+        static int shield;
+        static int maxShield = 100;
+        static int lives;
+        static int MaxLives = 99;
+        static int lvl;
+        static int maxLvl = 99;
+        static int xp;
+        static int xpToLvlUp = 100;
+
         static void Main(string[] args)
         {
+
+        }
+
+        static void Reset()
+        {
+            health = maxHealth;
+            shield = maxShield;
+            lives = 3;
+            lvl = 1;
+            xp = 0;
         }
     }
 }
