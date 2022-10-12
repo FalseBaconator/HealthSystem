a139f8afa0bacefe1d01353717863998762db510
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-10-12 14:08:50 -0300

    made DetermineStatus() and ShowHud()

--- HealthSystem/Program.cs
+++ HealthSystem/Program.cs
@@ -33,5 +33,52 @@ namespace HealthSystem
             lvl = 1;
             xp = 0;
         }
+
+        static string DetermineStatus()
+        {
+            string status;
+
+            if(health >= 0 && health <= 10)
+            {
+                status = "Go to a hospital";
+            }else if(health <= 25)
+            {
+                status = "Probably crippled";
+            }else if(health <= 50)
+            {
+                status = "Doing pretty poorly";
+            }else if(health <= 75)
+            {
+                status = "You can probably walk that off right?";
+            }else if(health < 100)
+            {
+                status = "Doing alright";
+            }else if (health == 100)
+            {
+                status = "In perfect health";
+            }
+            else
+            {
+                status = "Something went wrong in DetermineStatus()";
+            }
+
+            return status;
+
+        }
+
+        static void ShowHud()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Shield: " + shield);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Status: " + DetermineStatus());
+            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("-----------------------");
+            Console.WriteLine();
+
+        }
+
+
     }
 }
